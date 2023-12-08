281481bb3df634fe75b9c06b262d82710056c99e
Şafak Dinç <114588752+kodsilici02@users.noreply.github.com>
2023-12-08 16:46:18 +0300

    OrderSidebar z-index problem fixed

--- components/Sidebar/OrderSidebar.vue
+++ components/Sidebar/OrderSidebar.vue
@@ -1,10 +1,11 @@
 <template>
   <div
     class="fixed top-0 right-0 md:sticky md:top-28 w-52 filter-height p-2"
-    style="box-shadow: rgba(0, 0, 0, 0.1) 3px 3px 8px 0px inset; z-index: 10"
-    :style="{ 'z-index': open ? 1001 : 10 }">
+    style="box-shadow: rgba(0, 0, 0, 0.1) 3px 3px 8px 0px inset"
+    :style="{ 'z-index': z_index }">
     <div class="text-2xl flex items-center gap-1 z-[1000]">
       <ClientOnly><font-awesome :icon="['fas', 'dollar-sign']" /></ClientOnly>
+      {{ z_index }}
       <div>999</div>
     </div>
     <div class="w-full flex flex-wrap items-center gap-x-1 z-[1000]">
@@ -46,8 +47,22 @@
 <script setup>
 import { ref, watch, onMounted } from 'vue';
 import { addDays, format } from 'date-fns';
+import { useSidebarStore } from '@/stores/sidebar';
+import { storeToRefs } from 'pinia';
+
+const store = storeToRefs(useSidebarStore());
 
 const emits = defineEmits(['changeAddress']);
+
+const z_index = computed(() => {
+  if (store.isWindowSmall.value) {
+    return 1000;
+  } else if (!store.isWindowSmall.value && open.value) {
+    return 1000;
+  } else {
+    return 10;
+  }
+});
 
 const open = ref(false);
 
